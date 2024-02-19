dbbc364a8b85390a1ebf290cd8a8711c8d021b1f
Ibrahim Taher <ibrahimtaher.sp@gmail.com>
2024-02-19 02:44:40 +0200

    NY-1 Setup project
    - Create react app
    - Install packages
    - Add assets
    - Remove unused files

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Logo = () => {
+  return (
+    <img
+      className="logo"
+      width="20"
+      height="25"
+      src="/assets/logo.png"
+      alt="NY Logo"
+    />
+  );
+};
